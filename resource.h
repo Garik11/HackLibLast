b3e8228a14291e5bf2603219a0749bbaf66bda67
Garik11 <58850520+Garik11@users.noreply.github.com>
2023-07-21 11:29:16 +0300

    Update resource.h

--- resource.h
+++ resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// Включаемый файл, созданный в Microsoft Visual C++.
-// Используется HackLibProject.rc
+// Р’РєР»СЋС‡Р°РµРјС‹Р№ С„Р°Р№Р», СЃРѕР·РґР°РЅРЅС‹Р№ РІ Microsoft Visual C++.
+// РСЃРїРѕР»СЊР·СѓРµС‚СЃСЏ HackLibProject.rc
 //
 #define IDI_ICON1                       101
 // Next default values for new objects
